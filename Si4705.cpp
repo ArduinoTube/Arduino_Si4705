2c3777a919ef830b65e9242407e543dfff9a0e31
ArduinoTube <savassaffak@hotmail.de>
2017-02-02 20:06:39 +0100

    Update Si4705.cpp

--- Si4705.cpp
+++ Si4705.cpp
@@ -4,7 +4,7 @@
 
 /*
 Si4705 Bibliothek
-Version:   1.0-04092016
+Version:   1.1-01022017
 Ersteller: ArduinoTube
 
 ###########################FM Radio###########################
@@ -25,7 +25,7 @@ Si4705::Si4705()
 /*******************************************************
 *----------Si4705 Initialisieren fuer Empfang----------* 
 ********************************************************/
-void Si4705::initRadio(int Power, int volume, int resetPin, int intPin, int auxPin, unsigned FMchannel)
+void Si4705::initRadio(int Power, int volume, int resetPin, int intPin, int auxPin, unsigned int FMchannel)
 {
 	Wire.begin();
 	pinMode(resetPin, OUTPUT);
@@ -33,23 +33,27 @@ void Si4705::initRadio(int Power, int volume, int resetPin, int intPin, int auxP
 	pinMode(auxPin,   INPUT);
 	resetRadio(resetPin);
 	digitalWrite(intPin,HIGH);  
+	initFM();
 	if(Power==FM)
 	{
-	  initFM();
+	  seekLimit(channelTOP, channelBTM);
+	  seekStep(_100kHzSeek);
+	  seekThreshold(seekSNR,seekRSSI);
+	  setFMDeemph(Deemphasis);
 	  setChFilter(ChFilter40);
 	  setHiCut_Off(HiCutDis_MaxAudioTrans,_6kHz,SNRHighThreshold,SNRLowThreshold);
 	  setSNC(StereoRSSIThrold,MonoRSSIThrold,StereoSNRThrhold,MonoSNRThrhold);
-	  setFMDeemph(Deemphasis);
-	  delay(1000);
-	  setVolume(volume);
+	  delay(500);
 	  setFM(FMchannel);
+	  setVolume(volume);
 	  digitalWrite(auxPin,LOW);
+	  audioMute(muteOFF);
 	}
-	else
+	else if(Power==LINE)
 	{
 	  digitalWrite(auxPin,HIGH);
+	  audioMute(muteOFF);
 	}
-	audioMute(muteOFF);
 }
 
 /*******************************************************
@@ -76,17 +80,29 @@ void Si4705::audioMute(uint8_t OnOff)
 /*******************************************************
 *-----------------FM Channel Filter--------------------*
 *******************************************************/
-void Si4705::setChFilter (int channelFilter)
+void Si4705::setChFilter (int channelFilter_)
 {
 	Wire.beginTransmission(Si4705_Addr);
 	Wire.write(setProperty1);
 	Wire.write(setProperty2);	
 	Wire.write(channelFilter1);
 	Wire.write(channelFilter2);
-	Wire.write(channelFilter >> 8);
-	Wire.write(channelFilter&0xFF);
+	Wire.write(channelFilter_ >> 8);
+	Wire.write(channelFilter_&0xFF);
 	Wire.endTransmission();
 	delay(10);
+	channelFilter=channelFilter_;
+}
+
+void Si4705::autoChFilter (void)
+{
+  if((SNR <   8)&&(channelFilter==ChFilter60))			 	setChFilter(ChFilter40);
+  if((SNR <  10)&&(channelFilter==ChFilter84))			 	setChFilter(ChFilter60);
+  if((SNR <  12)&&(channelFilter==ChFilter110))			 	setChFilter(ChFilter84);
+  
+  if((SNR >  14)&&(channelFilter==ChFilter40))				setChFilter(ChFilter60);
+  if((SNR >  18)&&(channelFilter==ChFilter60))				setChFilter(ChFilter84);
+  if((SNR >  22)&&(channelFilter==ChFilter84))				setChFilter(ChFilter110);
 }
 
 /*******************************************************
@@ -130,6 +146,7 @@ void Si4705::setHiCut_Off (int maxFreq, int Freq, int SNRHigh, int SNRLow)
 *******************************************************/
 void Si4705::setFMDeemph (int Deemphasis)
 {
+	
 	Wire.beginTransmission(Si4705_Addr);
 	Wire.write(setProperty1);
 	Wire.write(setProperty2);
@@ -146,6 +163,7 @@ void Si4705::setFMDeemph (int Deemphasis)
 *******************************************************/
 void Si4705::setAntenna (int FM_ANTENNA)
 {
+	
 	Wire.beginTransmission(Si4705_Addr);
 	Wire.write(setProperty1);
 	Wire.write(setProperty2);
@@ -193,6 +211,70 @@ void Si4705::initFM (void)
 
 }
 
+/*******************************************************
+*----------------Suchgrenzen einstellen----------------* 
+********************************************************/
+void Si4705::seekThreshold(int seekSNR, int seekRSSI)
+{
+	Wire.beginTransmission(Si4705_Addr);
+    Wire.write(setProperty1);
+    Wire.write(setProperty2);
+    Wire.write(SeekSNRProperty);
+    Wire.write(SeekSNRProperty1);
+	Wire.write(SeekSNRProperty2);
+	Wire.write(seekSNR&0x7F);
+	Wire.endTransmission();
+	delay(50);
+
+	Wire.beginTransmission(Si4705_Addr);
+    Wire.write(setProperty1);
+    Wire.write(setProperty2);
+    Wire.write(SeekRSSIProperty);
+    Wire.write(SeekRSSIProperty1);
+	Wire.write(SeekRSSIProperty2);
+	Wire.write(seekRSSI&0x7F);
+	Wire.endTransmission();
+	delay(50);
+}
+
+void Si4705::seekStep (int seekStep)
+{
+	Wire.beginTransmission(Si4705_Addr);
+    Wire.write(setProperty1);
+    Wire.write(setProperty2);
+    Wire.write(SeekStepProperty);
+    Wire.write(SeekStepProperty1);
+	Wire.write(SeekStepProperty2);
+	Wire.write(seekStep&0x1F);
+	Wire.endTransmission();
+	delay(50);	
+}
+
+/*******************************************************
+*----------------Suchgrenzen einstellen----------------* 
+********************************************************/
+void Si4705::seekLimit(unsigned int seekTop, unsigned int seekBottom)
+{
+	Wire.beginTransmission(Si4705_Addr);
+    Wire.write(setProperty1);
+    Wire.write(setProperty2);
+    Wire.write(SeekBottomProperty);
+    Wire.write(SeekBottomProperty1);
+	Wire.write(seekBottom >> 8);
+	Wire.write(seekBottom&0xFF);
+	Wire.endTransmission();
+	delay(50);
+	
+	Wire.beginTransmission(Si4705_Addr);
+    Wire.write(setProperty1);
+    Wire.write(setProperty2);
+    Wire.write(SeekTopProperty);
+    Wire.write(SeekTopProperty1);
+	Wire.write(seekTop >> 8);
+	Wire.write(seekTop&0xFF);
+	Wire.endTransmission();
+	delay(50);	
+}
 
 /*******************************************************
 *-----Referenz-Taktquelle festlegen und einstellen-----* 
@@ -207,7 +289,7 @@ void Si4705::setRefClock (void)
     Wire.write(intRefClock1);
     Wire.write(intRefClock2);
     Wire.endTransmission();
-    delay(500);
+    delay(100);
     
     Wire.beginTransmission(Si4705_Addr);
     Wire.write(setProperty1);
@@ -217,8 +299,9 @@ void Si4705::setRefClock (void)
     Wire.write(QuarzFreq1);     
     Wire.write(QuarzFreq2);
     Wire.endTransmission();
-    delay(500);
+    delay(100);
 }
+
 /*******************************************************
 *------------FM Empfangsfrequenz einstellen------------* 
 ********************************************************/
@@ -226,6 +309,7 @@ void Si4705::setFM (unsigned int FMchannel)
 { 
    if(FMchannel>channelTOP)FMchannel=channelBTM;
    if(FMchannel<channelBTM)FMchannel=channelTOP;
+   if(channelFilter!=ChFilter40)setChFilter(ChFilter40);
    Wire.beginTransmission(Si4705_Addr);
    Wire.write(setFMchannel1);
    Wire.write(setFMchannel2);
@@ -233,17 +317,60 @@ void Si4705::setFM (unsigned int FMchannel)
    Wire.write(FMchannel&0xFF);
    Wire.write(setFMchannel3);
    Wire.endTransmission();
-   delay(50);
-   readData();
+   delay(10);
+}
+
+/*******************************************************
+*--------------FM Stereo/Mono Umschalten---------------* 
+********************************************************/
+void Si4705::forceMono (int forceMono)
+{
+	if(forceMono==true) setSNC(127,126,127,126);
+	if(forceMono==false)setSNC(StereoRSSIThrold,MonoRSSIThrold,StereoSNRThrhold,MonoSNRThrhold);
 }
 
 /*******************************************************
 *-------------------FM Sendersuchlauf------------------* 
 ********************************************************/
-/* unsigned Si4705::seekStation (int Direction)
+void Si4705::seekStation (int Direction, unsigned int &channel)
 {
-	
-} */
+	audioMute(muteON);
+    clearRDS();
+	int seekStation = true;
+	int oldStation  = channel;
+    while(seekStation == true)
+    {
+      channel += Direction * 10;
+      if (channel > 10800)channel = 8750;
+      if (channel < 8750) channel = 10800;
+      setFM(channel);
+	  delay(110);seekData();
+      if((AFC&(1<<0))||(oldStation == channel))
+	  seekStation = false;
+    }
+    audioMute(muteOFF);	
+}
+
+/*******************************************************
+*----------------FM Sendersuchlauf Si4735--------------* 
+********************************************************/
+void Si4705::seekAuto (int Direction, unsigned int &channel)
+{
+	AFC = 0;
+	audioMute(muteON);
+    clearRDS(); 
+    Wire.beginTransmission(Si4705_Addr);
+    Wire.write(seekStationCmd);
+	if(Direction>0)Wire.write(seekUpCmd);
+	if(Direction<0)Wire.write(seekDownCmd);
+	Wire.endTransmission();
+    delay(10);
+	while(!AFC){seekData();delay(200);}
+	audioMute(muteOFF);
+	delay(1000);
+	seekData();
+	channel = CHANNEL;
+}
 
 /*******************************************************
 *Set Stereo/Mono Threshold fuer SNC abh. von RSSI / SNR*
@@ -252,43 +379,43 @@ void Si4705::setSNC (int StereoThreshold, int MonoThreshold, int StereoSNR, int 
 {
 	Wire.beginTransmission(Si4705_Addr);
 	Wire.write(setProperty1);
-    	Wire.write(setProperty2);
+    Wire.write(setProperty2);
 	Wire.write(StMoRSSIProperty);
 	Wire.write(SterRSSIProperty1);
 	Wire.write(SterRSSIProperty2);
 	Wire.write(StereoThreshold);
-    	Wire.endTransmission();
-    	delay(10);
+    Wire.endTransmission();
+    delay(10);
    
 	Wire.beginTransmission(Si4705_Addr);
 	Wire.write(setProperty1);
-    	Wire.write(setProperty2);
+    Wire.write(setProperty2);
 	Wire.write(StMoRSSIProperty);
 	Wire.write(MonoRSSIProperty1);
 	Wire.write(SterRSSIProperty2);
 	Wire.write(MonoThreshold);
 	Wire.endTransmission();
-    	delay(10);
+    delay(10);
 	
 	Wire.beginTransmission(Si4705_Addr);
 	Wire.write(setProperty1);
-    	Wire.write(setProperty2);
+    Wire.write(setProperty2);
 	Wire.write(StMoSNRProperty);
 	Wire.write(SterSNRProperty1);
 	Wire.write(SterSNRProperty2);
 	Wire.write(StereoSNR);
-    	Wire.endTransmission();
-    	delay(10);
+    Wire.endTransmission();
+    delay(10);
    
 	Wire.beginTransmission(Si4705_Addr);
 	Wire.write(setProperty1);
-   	Wire.write(setProperty2);
+    Wire.write(setProperty2);
 	Wire.write(StMoSNRProperty);
 	Wire.write(MonoSNRProperty1);
 	Wire.write(MonoSNRProperty2);
 	Wire.write(MonoSNR);
 	Wire.endTransmission();
-    	delay(10);
+    delay(10);
 }
 
 /*******************************************************
@@ -330,13 +457,12 @@ void Si4705::readRDS (void)
   Wire.write(getRDS1);
   Wire.write(getRDS2);
   Wire.endTransmission();
-  delay(1);
+  delay(10);
   Wire.requestFrom(Si4705_Addr, 32);
-  for(int Times=0; ((Times<12)&&(Wire.available())); Times++)
+  for(int Times=0; ((Times<=12)&&(Wire.available())); Times++)
   RDS[Times] = Wire.read();
   Wire.endTransmission();
-  delay(39);
-  RDSflag   = RDS[1]&1;
+  delay(40);
 }
 
 /*******************************************************
@@ -346,7 +472,7 @@ void Si4705::decodePI (void)
 {
 	int GroupType = RDS[6]>>4;
 	int RDSSynch  = RDS[2]&1;
-	if((GroupType==0)&&(RDSSynch))
+	if((GroupType==0)&&(RDSSynch==1))
 	{
 		if(RDS[4])PICODE[0]=RDS[4];
 		if(RDS[5])PICODE[1]=RDS[5];
@@ -384,14 +510,14 @@ void Si4705::decodePS (void)
 ********************************************************/
 void Si4705::decodeRT (void)
 {
-  int GroupType = RDS[6]>>4;
-  int RDSSynch  = RDS[2]&1;
-  int RTAdress  = 0;
-  int LZA       = 0;
-  int LZE       = 0;
-  int strl      = 0;
-  int BLER		= RDS[12];
-  if((GroupType==2)&&(RT_decodeProgress<2)&&(RTflag==false)&&(RDSSynch))
+  volatile uint8_t GroupType = RDS[6] >> 4;
+  volatile uint8_t RDSSynch  = RDS[2]&1;
+  volatile uint8_t RTAdress  = 0;
+  volatile uint8_t LZA       = 0;
+  volatile uint8_t LZE       = 0;
+  volatile uint8_t strl      = 0;
+  volatile uint8_t BLER		 = RDS[12];
+  if((GroupType==2)&&(RT_decodeProgress<2)&&(RDSSynch==1)&&(RTflag==false))
   {
       RTAdress=RDS[7]&15;
       RTAdress*=4;
@@ -400,27 +526,32 @@ void Si4705::decodeRT (void)
          RT_decodeProgress=1;
          for(int Clear=0; Clear<=64; Clear++)RTTemp[Clear]= 0;
       }
-      strl=strlen(RTTemp);
-      if((RT_decodeProgress==1)&&(strl!=0)&&(RTAdress==0))
+	  strl=strlen(RTTemp);
+      if((RT_decodeProgress==1)&&(strl)&&(RTAdress<oldRTcount))
       {
-          for(int Clear=0; Clear<=64; Clear++)RT[Clear]    = 0;
-          for(LZE=64;     RTTemp[LZE]<=32; LZE--);
+		  for(int Clear=0; Clear<=64; Clear++)RT[Clear] = 0;
           for(LZA=0;      RTTemp[LZA]<=32; LZA++);
-          
-          for(int copy = LZA; (copy <= LZE); copy++)RT[copy] = RTTemp[copy];
-          for(int Clear=0; Clear<=64; Clear++)      RTTemp[Clear]= 0;
+          for(LZE=64;      RTTemp[LZE]<=32; LZE--);
+		  
+          for(int copy = LZA; copy<=LZE; copy++)
+		  {
+			  RT[copy-LZA] = RTTemp[copy];
+			  RT[copy-LZA+1] ='\0';
+		  }
+          for(int Clear=0; Clear<=64; Clear++)RTTemp[Clear]= 0;
           RT_decodeProgress=0;
           RTflag = true;
+		  oldRTcount=0;
       }
-      if((BLER)&&(RT_decodeProgress==1))RT_decodeProgress=0;
+	  oldRTcount=RTAdress;
+	  //if((BLER)&&(RT_decodeProgress==1))RT_decodeProgress=0;
       if(RT_decodeProgress==1)
-      {
+      {	  
           RTTemp[RTAdress]   = RDS[ 8];
           RTTemp[RTAdress+1] = RDS[ 9];
           RTTemp[RTAdress+2] = RDS[10];
           RTTemp[RTAdress+3] = RDS[11];
-          RTTemp[RTAdress+4]='\0';
-          RTAdress=0;
+		  RTTemp[RTAdress+4] = '\0';
       }
   }
 }
@@ -440,28 +571,28 @@ void Si4705::decodePTY (void)
   } 
   switch(_PTY)
   {
-    case  0:strcpy(PTY,"nicht Def\0");break;
-    case  1:strcpy(PTY,"Nachricht\0");break;
-    case  2:strcpy(PTY,"Aktuell  \0");break;
-    case  3:strcpy(PTY,"Informat.\0");break;
-    case  4:strcpy(PTY,"Sport    \0");break;
-    case  5:strcpy(PTY,"Bildung  \0");break;
+	case  0:strcpy(PTY,"unb. Format \0");break;
+    case  1:strcpy(PTY,"Nachrichten \0");break;
+    case  2:strcpy(PTY,"Aktuelles   \0");break;
+    case  3:strcpy(PTY,"Information \0");break;
+    case  4:strcpy(PTY,"Sport       \0");break;
+    case  5:strcpy(PTY,"Bildung     \0");break;
 	
-    case  6:{strcpy(PTY,"H_rspiel \0"); 
-	     PTY[1]=char(239);}      break;
+    case  6:{strcpy(PTY,"H_rspiel   \0"); 
+			PTY[1]=char(239);}		break;
     
-    case  7:strcpy(PTY,"Kultur   \0");break;
-    case  8:strcpy(PTY,"Wissensch\0");break;
-    case  9:strcpy(PTY,"Wortprogr\0");break;
-    case 10:strcpy(PTY,"Pop Musik\0");break;
-    case 11:strcpy(PTY,"Rock Musk\0");break;
-    case 12:strcpy(PTY,"Easy List\0");break;
-    case 13:strcpy(PTY,"L. Klass.\0");break;
-    case 14:strcpy(PTY,"Ernst Mus\0");break;
-    case 15:strcpy(PTY,"Andere Mu\0");break;
-    case 31:strcpy(PTY,"Alarm    \0");break;
+	case  7:strcpy(PTY,"Kultur      \0");break;
+    case  8:strcpy(PTY,"Wissenschaft\0");break;
+    case  9:strcpy(PTY,"Wortprogramm\0");break;
+    case 10:strcpy(PTY,"Pop Musik   \0");break;
+    case 11:strcpy(PTY,"Rock Musik  \0");break;
+    case 12:strcpy(PTY,"Easy Listen.\0");break;
+    case 13:strcpy(PTY,"Light Class.\0");break;
+    case 14:strcpy(PTY,"Ernste Musik\0");break;
+    case 15:strcpy(PTY,"Andere Musik\0");break;
+	case 31:strcpy(PTY,"Alarm       \0");break;
   }
-  if(!RDSSynch)strcpy(PTY,"kein PTY  \0");
+  if(!RDSSynch)strcpy(PTY,"Sendertyp nV\0");
   if(RDSSynch )PTYflag = true;
   else         PTYflag = false;
 }
@@ -550,11 +681,12 @@ void Si4705::decodeTC (void)
 {
   int GroupType = RDS[6]>>4;
   int RDSSynch  = RDS[2]&1;
-  if((GroupType==0)&&(RDSSynch))
+  if(GroupType==0)
   {
 	  if(((RDS[6]&(1<<2))==0)&&((RDS[7]&(1<<4)))) 		TC=1;	// EON is available
-	  if(((RDS[6]&(1<<2)))&&((RDS[7]&(1<<4))==0)) 		TC=2;	// TP Signal is available
-	  if(((RDS[6]&(1<<2)))&&((RDS[7]&(1<<4))))  		TC=3;	// Traffic announcement is now
+	  else if(((RDS[6]&(1<<2)))&&((RDS[7]&(1<<4))==0)) 	TC=2;	// TP Signal is available
+	  else if(((RDS[6]&(1<<2)))&&((RDS[7]&(1<<4))))  	TC=3;	// Traffic announcement is now
+	  else TC=0;
   }
 }
 
@@ -567,9 +699,9 @@ void Si4705::decodeAF (void)
 	int RDSSynch  = RDS[2]&1;
 	if((AF_Zaehler<40)&&(GroupType==0)&&(RDSSynch))
 	{
-		_Puffer0=((RDS[8]+875)*10);
-		_Puffer1=((RDS[9]+875)*10);
-		if ((_Puffer0>8750)&&(_Puffer0<=10800)&&(_Puffer1>8750)&&(_Puffer1<=10800))
+		_Puffer0=((RDS[8]+channelBTM/10)*10);
+		_Puffer1=((RDS[9]+channelBTM/10)*10);
+		if ((_Puffer0>channelBTM)&&(_Puffer0<=channelTOP)&&(_Puffer1>channelBTM)&&(_Puffer1<=channelTOP))
 		{
 			for (_AF_scan_a = 0; ((_AF_scan_a <= 40) && ((AF[_AF_scan_a] != _Puffer0)) && (_Puffer0 != _Puffer1)); _AF_scan_a ++);
 			for (_AF_scan_b = 0; ((_AF_scan_b <= 40) && ((AF[_AF_scan_b] != _Puffer1)) && (_Puffer0 != _Puffer1)); _AF_scan_b ++);
@@ -646,6 +778,7 @@ void Si4705::clearRDS (void)
   AF_trying = 0;
   _PTY=_PTY1=_PTY2=0;
   AF_PICODE[0]=AF_PICODE[1]=0;
+  oldRTcount=0;
   PICODE[0]=PICODE[1]=0;
   for(int clearRDS = 0; clearRDS <= 64; clearRDS++)
   {
@@ -662,14 +795,34 @@ void Si4705::clearRDS (void)
 /*******************************************************
 *--------------Lese Daten aus dem Receiver-------------* 
 ********************************************************/
+void Si4705::seekData (void)
+{ 
+  Wire.beginTransmission(Si4705_Addr);
+  Wire.write(FMTuneStatus2);
+  Wire.write(TuneReg);
+  Wire.endTransmission();
+  delay(5);
+
+  Wire.requestFrom(Si4705_Addr, 32);
+  for(int Times=0; ((Times<8)&&(Wire.available())); Times++)
+  Data[Times] = Wire.read();
+  RSSi      = Data[4];
+  SNR       = Data[5]; 
+  CHANNEL   = Data[2] << 8;
+  CHANNEL  += Data[3]&0xFF;
+  AFC       = Data[1];
+  TUNE_CAP  = Data[7];
+  delay(5);
+}
+
 void Si4705::readData (void)
 {
   Wire.beginTransmission(Si4705_Addr);
   Wire.write(FMTuneStatus1);
   Wire.write(TuneReg);
   Wire.endTransmission();
-  delay(10);
-  
+  delay(5);
+
   Wire.requestFrom(Si4705_Addr, 32);
   for(int Times=0; ((Times<8)&&(Wire.available())); Times++)
   Data[Times] = Wire.read();
@@ -679,24 +832,7 @@ void Si4705::readData (void)
   STBLEND   = Data[3]&0x7F;
   OFFSET    = Data[7];
   Wire.endTransmission();
-  delay(10);
-  
-  Wire.beginTransmission(Si4705_Addr);
-  Wire.write(FMTuneStatus2);
-  Wire.write(TuneReg);
-  Wire.endTransmission();
-  delay(10);
-
-  Wire.requestFrom(Si4705_Addr, 32);
-  for(int Times=0; ((Times<8)&&(Wire.available())); Times++)
-  Data[Times] = Wire.read();
-/*   RSSi      = Data[4];
-  SNR       = Data[5]; */  
-  CHANNEL   = Data[2] >> 8;
-  CHANNEL  += Data[3]&0xFF;
-  AFC       = Data[1];
-  TUNE_CAP  = Data[7];
-  delay(10);
+  delay(5);
 }
 
 /**********************************************************
